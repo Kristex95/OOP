2ea75c4721cdfbdfc6f387a3968eed63f96bb2d1
Kristex95 <kristex95@gmail.com>
2021-09-22 23:29:53 +0300

    find_by_data function added

--- List_1 Test_4/List_1 Test_4/List_1 Test_4.cpp
+++ List_1 Test_4/List_1 Test_4/List_1 Test_4.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 template <typename T>
 class List {
+
 	template <typename T>
 	class Node {
 
@@ -10,6 +11,7 @@ class List {
 		T data;
 		Node* pNext;
 
+		//Node constructor
 		Node(T data = T(), Node* pNext = nullptr) {
 			this->data = data;
 			this->pNext = pNext;
@@ -19,25 +21,11 @@ class List {
 	int Size;
 	Node<T>* head;
 public:
-	List();
-	~List();
-
-	template <typename T>
-	void push_back(T data) {
-		if (head == nullptr) {
-			head = new Node<T>(data);
-		}
-		else {
-			Node<T>* current = this->head;
-			while (current->pNext != nullptr) {
-				current = current->pNext;
-			}
-			current->pNext = new Node<T>(data);
-
-		}
-		
-		this->Size++;
-	}
+	List();						//List constructor			
+	~List();					//List destructor
+	void push_back(T data);		//adds new element to the end of the list
+	T& operator[](int index);	//gets the element by some index
+	int find_by_data(T data);
 };
 
 template <typename T>
@@ -46,16 +34,66 @@ List<T>::~List() {
 }
 
 template <typename T>
-List<T>::List() {
+List<T>::List() {								
 	Size = 0;
 	head = NULL;
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)					//if the list is empty
+	{					
+		head = new Node<T>(data);
+	}
+	else									//if the list is not empty
+	{									
+		Node<T>* current = this->head;
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+		current->pNext = new Node<T>(data);
+	}
+	this->Size++;
+}
+
+template <typename T>
+T& List<T>::operator[](int index) {
+	int counter = 0;
+	Node<T>* current = this->head;
+	while (current != nullptr)
+	{
+		if (counter == index)
+		{
+			return current->data;
+		}
+		current = current->pNext;
+		counter++;
+	}
+}
+
+template<typename T>
+int List<T>::find_by_data(T data)
+{
+	Node<T>* current = head;
+	int counter = 0;
+	while (current != nullptr) {
+		if (current->data == data) {
+			return counter;
+		}
+		current = current->pNext;
+		counter++;
+	}
+	cout << "There is no data: " << data << endl;
+	return 0;
 }
 
 int main() {
 	List<int> list;
 	for (int i = 0; i < 10; i++) {
-		list.push_back(i);
+		list.push_back(i+2);
 	}
-	
+	cout << "Data index: " << list.find_by_data(5);
 	return 1;
 }
