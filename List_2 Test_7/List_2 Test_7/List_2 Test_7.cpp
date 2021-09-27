1ce1d5fd1d40f3bb27477c070962552e40077c7e
Kristex95 <kristex95@gmail.com>
2021-09-27 22:00:12 +0300

    List_2 Test_7 started

---
+++ List_2 Test_7/List_2 Test_7/List_2 Test_7.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class CIDR_IPv4 {
+	uint8_t IP[4];
+	uint8_t mask;
+public:
+	void Set_IP();
+	CIDR_IPv4() : IP() {
+		Set_IP();
+	}
+};
+
+class IPv6 {
+	uint16_t IP[8];
+
+public:
+	void Set_IP();
+	IPv6() : IP() {
+		Set_IP();
+	}
+};
+
+int main()
+{
+	//IPv6 ipv6;
+	CIDR_IPv4 ipv4;
+	return 1;
+}
+
+void IPv6::Set_IP() {
+	cout << "Enter new IP adress: ";
+	string buff;
+	for (int i = 0; i < 7; i++) {
+		getline(cin, buff, ':');
+		IP[i] = stoul(buff, nullptr, 16);
+	}
+	getline(cin, buff);
+	IP[7] = stoul(buff, nullptr, 16);
+}
+
+void CIDR_IPv4::Set_IP()
+{
+	cout << "Enter new IP adress: ";
+	string buff;
+	for (int i = 0; i < 3; i++) {
+		getline(cin, buff, '.');
+		int byte = stoi(buff);
+		if (byte >= 256) {
+			byte = 255;
+		}
+		else if (byte < 0) {
+			byte = 0;
+		}
+		IP[i] = byte;
+	}
+	getline(cin, buff);
+	int byte = stoi(buff);
+	if (byte >= 256) {
+		byte = 255;
+	}
+	else if (byte < 0) {
+		byte = 0;
+	}
+	IP[3] = byte;
+}
